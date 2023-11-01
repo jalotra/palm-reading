dddb79d199758455289b189518472aeee34e5d22
jalotra <jalotrashivam9@gmail.com>
2023-11-02 00:43:35 +0530

    added notes

--- src/gpt/gpt.py
+++ src/gpt/gpt.py
@@ -2,15 +2,22 @@
 import openai 
 import os 
 import pdf2text
+import logging
 
+# logging support
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+chat_completion = openai.ChatCompletion()
 
 def prompt():
-    return '''You are great at summarization, I have a bunch of text files that I want to find rules for for.
-    The text is about indian palmistry, I want you to summarize the text and find rules in the files !'''
+    return '''
+        You are great at summarization, I have a bunch of text files that I want to find rules for for.
+        The text is about indian palmistry, I want you to summarize the text and find rules in the files !
+        '''
 
 
 
-def pdf2text():
+def pdf_to_text():
     """
     @returns : reads all the pdfs files present in ./pdfs and returns data
     """
@@ -26,4 +33,12 @@ def pdf2text():
 
 
 if __name__ == "__main__":
+    pdf_to_text_data = pdf_to_text()
+    for obj in pdf_to_text_data:
+        filepath, data = obj.get("filepath"), obj.get("filepath")
+        if len(data) == 0:
+            raise AssertionError(f"the pdf present in location : {filepath} is not having any data ? check once !")
 
+        logger.info(f"Total Pages in the file : {filepath} are {len(data)}")
+        for idx, page in enumerate(data):
+            openai.ChatCompletion()        
